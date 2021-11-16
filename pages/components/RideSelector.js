98ec6c14bf4df60a0f3031cf26618585011e28db
Ayomide Adedeji <diamonditadedeji@gmail.com>
2021-11-16 17:21:13 +0100

    finished the app

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -5,11 +5,13 @@ import { carList } from '../data/carList'
 
 
 const RideSelector = ({pickupCoordinates, dropoffCoordinates}) => {
-    const [rideDuration, setRideDuration] = useState(0)
+    const [ setRideDuration] = useState(0)
 
 
 // Getting Ride duration from Mapbox API
     useEffect(() => {
+        const [ rideDuration] = useState(0)
+
         rideDuration = fetch(
             `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoiZGlhbW9uZGF5byIsImEiOiJja3ZucDQ5dGk3bm1nMzFxd3F3eWkycTZ3In0._fWUYJdKFxCNA9Db9vhieA`
         )
